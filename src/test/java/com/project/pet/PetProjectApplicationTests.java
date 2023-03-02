a8df1e31f4b12524a056797156def0e43ab83fe1
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-02 23:08:09 +0200

    commit

--- src/test/java/com/project/pet/PetProjectApplicationTests.java
+++ src/test/java/com/project/pet/PetProjectApplicationTests.java
@@ -1,13 +1,17 @@
 package com.project.pet;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class PetProjectApplicationTests {
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 }
