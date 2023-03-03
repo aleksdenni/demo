a4a0db9832544f9e21519902a5fe6e22da0081ca
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-03 22:40:42 +0200

    just commit

--- src/main/java/com/project/pet/model/User.java
+++ src/main/java/com/project/pet/model/User.java
@@ -2,20 +2,23 @@ package com.project.pet.model;
 
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "users")
 public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id", unique = true, nullable = false, updatable = false)
+    @Column(name = "user_id", unique = true, nullable = false)
     private Long id;
-
-/*    @Column(name = "username", unique = true, nullable = false)
-    private String userName;*/
 
     @Column(name = "first_name", nullable = false)
     private String firstName;
@@ -25,4 +28,17 @@ public class User{
 
     @Column(name = "email", unique = true, nullable = false)
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
