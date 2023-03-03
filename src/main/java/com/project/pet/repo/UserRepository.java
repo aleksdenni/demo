86e120d86feac6b849b46f47112f5535f8dbaf1d
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-03 23:12:33 +0200

    just commit

--- src/main/java/com/project/pet/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.pet.repo;
-
-import com.project.pet.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/test/java/com/project/pet/PetProjectApplicationTests.java
+++ src/test/java/com/project/pet/PetProjectApplicationTests.java
@@ -3,11 +3,9 @@ package com.project.pet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class PetProjectApplicationTests {
 	@Test
