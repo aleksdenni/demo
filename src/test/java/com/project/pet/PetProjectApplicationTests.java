5047f2e7b2d11447116fc0ec612594e892bc9d27
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-03 10:22:17 +0200

    next commit

--- src/test/java/com/project/pet/PetProjectApplicationTests.java
+++ src/test/java/com/project/pet/PetProjectApplicationTests.java
@@ -2,10 +2,7 @@ package com.project.pet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.project.pet.controllers.GreetingController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,12 +10,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @SpringBootTest
 class PetProjectApplicationTests {
-	@Autowired
 	@Test
 	void contextLoads(ApplicationContext context) {
 		assertThat(context).isNotNull();
 	}
-//	void contextLoads(ApplicationContext context) {
-//		assertThat(context.getBean(GreetingController.class)).isNotNull();
-//	}
 }
