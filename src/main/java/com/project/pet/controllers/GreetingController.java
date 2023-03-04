1797672dfe1bac2271c4641ddd9901407eb0e31c
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-04 10:16:48 +0200

    Hello from my app

--- src/main/java/com/project/pet/controllers/GreetingController.java
+++ src/main/java/com/project/pet/controllers/GreetingController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, %s!";
+    private static final String template = "Greetings, from %s!";
     private final AtomicLong counter = new AtomicLong();
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "my app") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
